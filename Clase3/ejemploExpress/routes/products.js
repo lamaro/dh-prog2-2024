613db4e5e36e0f055453c40f7ea0a6175211c6b1
Leandro Amaro Garcia <leandroamarogarcia@Leandros-MacBook-Pro.local>
2024-03-18 15:25:12 -0300

    added color view

--- Clase3/ejemploExpress/routes/products.js
+++ Clase3/ejemploExpress/routes/products.js
@@ -4,6 +4,7 @@ const productController = require('../controllers/productController')
 
 router.get("/", productController.index);
 router.get("/brand/:brand", productController.brand);
+router.get("/color/:color", productController.color)
 
 
 module.exports = router;
